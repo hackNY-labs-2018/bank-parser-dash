faedb1235203c317973778b3e8555785d3de41b4
cktang88 <arctangent20@yahoo.com>
2018-06-29 01:16:43 -0400

    Import file csv, display as table

--- README.md
+++ README.md
@@ -7,7 +7,13 @@ Dev
 
 Install:
 
+For Windows, first install [miniconda](https://conda.io/miniconda.html) (follow setup to create env also) and then install dash with conda:
+
+    conda install -c conda-forge pandas dash dash-html-components dash-core-components
+
+Other:
     pip install -r requirements.txt
+    pip install pandas
 
 Running:
 
--- app.py
+++ app.py
@@ -6,9 +6,27 @@ main python script
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
 
+# initialize dash app
 app = dash.Dash()
 
+# read from csv
+df = pd.read_csv('./data/hello.csv')
+
+# generates a table (reusable component)
+def generate_table(dataframe, max_rows=10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
+# defines front-end layout
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
 
@@ -16,6 +34,10 @@ app.layout = html.Div(children=[
         Dash: A web application framework for Python.
     '''),
 
+    # table
+    generate_table(df),
+
+    # graph
     dcc.Graph(
         id='example-graph',
         figure={
@@ -30,5 +52,6 @@ app.layout = html.Div(children=[
     )
 ])
 
+# starts app from command line, run Flask server
 if __name__ == '__main__':
     app.run_server(debug=True)
