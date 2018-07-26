fb79fd7efcf9c6edaf95d57e48f19380a7e13284
cktang88 <arctangent20@yahoo.com>
2018-07-26 01:26:49 -0400

    normalize shorter lines

--- parsing/exp_text_parser.py
+++ parsing/exp_text_parser.py
@@ -1,6 +1,8 @@
 """ EXPERIMENTAL Text Parser parses text directly from a PDF using the "pdfquery" library
 - WARNING: Does some crazy assumptions in attempt to correct comma placement
 - Trades generalizability for accuracy in niche case
+- Operates on the principle that the first two cells are always dates, highly accurate,
+and that the location and transaction name is the source of inaccuracy
 """
 
 import os
@@ -8,9 +10,6 @@ import re
 import sys
 
 import pdfquery
-
-LOC_INDEX = 3 # location(state) starts on 4th column, so zero-index = 3
-NORM_LENGTH = 8 # set norm
 
 def extract_transactions(pdf):
     """Extracts a list of all transactions in CSV format from a PDF
@@ -35,13 +34,6 @@ def extract_transactions(pdf):
             ])['transactions']
         while len(t) == 6:
             t = ','.join([process_text_cell(cell) for cell in t])
-            # post process normalization
-            arr = t.split(',')
-            if len(arr) > NORM_LENGTH:
-                li = LOC_INDEX
-                arr[li-1:li+1] = [' '.join(arr[li-1:li+1])]
-            t = ','.join(arr)
-            # continue...
             transactions.append(t)
             y -= line_height
             t = pdf.extract([
@@ -49,7 +41,47 @@ def extract_transactions(pdf):
                 ('transactions', 'LTTextLineHorizontal:in_bbox("%s,%s,%s,%s")' %
                  (x - 100, y - line_height, x + 500, y))
                 ])['transactions']
+    normalize_length(transactions)
     return '\n'.join(transactions)
+
+def normalize_length(transactions):
+    ''' smart post processing (ensure all lines exactly = NORM_LENGTH)
+    '''
+    LOC_INDEX = 3 # location(state) starts on 4th column, so zero-index = 3
+    NORM_LENGTH = 8 # set norm
+    loc_set = set() # store all locations seen before, no repeats
+    li = LOC_INDEX
+
+    ''' normalize long lines '''
+    for index, line in enumerate(transactions):
+        arr = line.split(',')
+        if len(arr) > NORM_LENGTH:
+            arr[li-1:li+1] = [' '.join(arr[li-1:li+1])]
+        loc_set.add(arr[li])
+        transactions[index] = ','.join(arr)
+    # these two for-loops cannot be combined b/c one must run in full before the other
+    loc_sorted = sorted(loc_set, key=lambda x: len(x), reverse=True) # sort locations by length
+
+    ''' normalize short lines '''
+    for index, line in enumerate(transactions):
+        arr = line.split(',')
+        if len(arr) >= NORM_LENGTH:
+            continue
+        # attempt to match longest locations first
+        for loc in loc_sorted:
+            if len(loc) == 0:
+                continue
+            cell = arr[li-1]
+            if not cell.endswith(loc):
+                continue
+            k = cell.rfind(loc)
+            arr[li-1] = cell[:k] # split by reverse indexOf
+            arr.insert(li, cell[k:])
+            break # max one match
+        while len(arr) < NORM_LENGTH:
+            # last resort: pad
+            arr.insert(li+1, '')
+        transactions[index] = ','.join(arr)
 
 def process_text_cell(cell):
     """Helper function to process each text cell extracted from a pdf
