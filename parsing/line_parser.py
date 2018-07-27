dffced8acac16ea78edab6757cf0f3074285a53f
Noah Huber-Feely <nhuberfeely@gmail.com>
2018-07-27 07:33:53 -0400

    add overlapping field support

--- parsing/line_parser.py
+++ parsing/line_parser.py
@@ -32,11 +32,54 @@ def special_data_from_raw_line(line, configuration):
                 if (j['x'] - last_x) >= space_threshold and (j['x'] - last_x) < space_threshold * 2:
                     field_data += ' ' # add in a space
                 field_data += j['contents']
-            last_x = j['x']
+                last_x = j['x']
         if i == 'amount':
             if len(field_data) >= 3 and not field_data[len(field_data)-4] == '.':
                 field_data = field_data[:len(field_data)-3] + '.' + field_data[len(field_data)-2:]
         formatted_line[i] = field_data
+
+    for i in configuration['overlapping']:
+        max_gap = 0
+        idx_split = -1
+        right_x = i['bounding'][0]
+        left_x = i['bounding'][1]
+        space_threshold = 20 * multiplier
+        last_x = -100
+        for idx, j in enumerate(line):
+            if j['x'] >= right_x and j['x'] < left_x:
+                if last_x > 0 and (j['x'] - last_x) >= max_gap and (j['x'] - last_x) > space_threshold:
+                    max_gap = (j['x'] - last_x)
+                    idx_split = idx
+            last_x = j['x']
+        line_chunks = [line[:idx_split], line[idx_split:]]
+        if idx_split <= 10:
+            line_chunks = [line, []]
+        # Process chunk 0
+        last_x = -100
+        field_data = ''
+        for j in line_chunks[0]:
+            if j['x'] >= right_x and j['x'] < left_x:
+                if (j['x'] - last_x) >= space_threshold and (j['x'] - last_x) < space_threshold * 2:
+                    field_data += ' ' # add in a space
+                field_data += j['contents']
+            last_x = j['x']
+        if i['fields'][0] == 'amount':
+            if len(field_data) >= 3 and not field_data[len(field_data)-4] == '.':
+                field_data = field_data[:len(field_data)-3] + '.' + field_data[len(field_data)-2:]
+        formatted_line[i['fields'][0]] = field_data
+        # Process chunk 0
+        last_x = -100
+        field_data = ''
+        for j in line_chunks[1]:
+            if j['x'] >= right_x and j['x'] < left_x:
+                if (j['x'] - last_x) >= space_threshold and (j['x'] - last_x) < space_threshold * 2:
+                    field_data += ' ' # add in a space
+                field_data += j['contents']
+            last_x = j['x']
+        if i['fields'][1] == 'amount':
+            if len(field_data) >= 3 and not field_data[len(field_data)-4] == '.':
+                field_data = field_data[:len(field_data)-3] + '.' + field_data[len(field_data)-2:]
+        formatted_line[i['fields'][1]] = field_data
 
     if is_line_accurate(formatted_line):
         return formatted_line
